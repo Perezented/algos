2a48d5fbe870ad05c8dc08ce7bc42a3bb5a246f9
Michael Perez <mperez6473@gmail.com>
2020-10-18 22:39:39 -0500

    works on low runtime problems

--- TwoSum.py
+++ TwoSum.py
@@ -28,10 +28,10 @@
 # Only one valid answer exists.
 
 
-numList1 = [11, 2, 15, 7]
-numList2 = [3, 2, 4]
-numList3 = [0, 4, 3, 0]
-numList4 = [2, 5, 5, 11]
+numList1 = [11, 2, 15, 7]   # to equal 9, [1, 3]
+numList2 = [3, 2, 4]    # to equal 6, [1, 2]
+numList3 = [0, 4, 3, 0]  # to equal 0, [0, 3]
+numList4 = [2, 5, 5, 11]  # to equal 10, [1, 2]
 
 
 def twoSum(nums, target):
@@ -49,55 +49,48 @@ def twoSum(nums, target):
                 index = 0
                 jindex += 1
         if jindex != index:
-            print([jindex, index])
             return [jindex, index]
         else:
-            print('jindex equaled index')
+            index += 1
+            return findSubNumbers(nums, index, jindex, currNum)
 
     return findSubNumbers(nums, index, jindex, currNum)
 
-    #     # start off, sub target with first jindex
-    #     if currNum - numsList[jindex] > 0:
-    #         currNum -= numsList[jindex]
-    #     elif currNum - numsList[jindex] < 0:
-    #         jindex += 1
-    #         return findSubNumbers(numsList, index, jindex, currNum)
-    #     elif currNum == 0:
-    #         if index == jindex and index < len(numsList)-1:
-    #             index += 1
-    #         if numsList[jindex]+numsList[index] != currNum:
-    #             index += 1
-    #         else:
-    #             return [jindex, index]
-    #         return findSubNumbers(numsList, index, jindex, currNum)
+#     # start off, sub target with first jindex
+#     if currNum - numsList[jindex] > 0:
+#         currNum -= numsList[jindex]
+#     elif currNum - numsList[jindex] < 0:
+#         jindex += 1
+#         return findSubNumbers(numsList, index, jindex, currNum)
+#     elif currNum == 0:
+#         if index == jindex and index < len(numsList)-1:
+#             index += 1
+#         if numsList[jindex]+numsList[index] != currNum:
+#             index += 1
+#         else:
+#             return [jindex, index]
+#         return findSubNumbers(numsList, index, jindex, currNum)
 
-    #     # sub from the next number in the index, next index != jindex
-    #     while index != len(numsList):
-    #         if index == jindex and index < len(numsList)-1:
-    #             index += 1
-    #         # looking for currNum to be zero
-    #         if (currNum - numsList[index]) == 0:
-    #             return [jindex, index]
-    #         index += 1
-    #     # if currNum minus next index != 0 then its not our num.
-    #     # if index is == numsList len, then move to next jindex
-    #     print(index, len(numsList))
-    #     if index == len(numsList):
-    #         index = 0
-    #         print('jindex b4: ', jindex)
-    #         if jindex < len(numsList)-1:
-    #             jindex += 1
-    #         print('jindex after: ', jindex)
-    #         currNum = target
-    #         return findSubNumbers(nums, index, jindex, currNum)
-    # return findSubNumbers(nums, index, jindex, currNum)
+#     # sub from the next number in the index, next index != jindex
+#     while index != len(numsList):
+#         if index == jindex and index < len(numsList)-1:
+#             index += 1
+#         # looking for currNum to be zero
+#         if (currNum - numsList[index]) == 0:
+#             return [jindex, index]
+#         index += 1
+#     # if currNum minus next index != 0 then its not our num.
+#     # if index is == numsList len, then move to next jindex
+#     if index == len(numsList):
+#         index = 0
+#         if jindex < len(numsList)-1:
+#             jindex += 1
+#         currNum = target
+#         return findSubNumbers(nums, index, jindex, currNum)
+# return findSubNumbers(nums, index, jindex, currNum)
 
 
-print('#'*10, 'first test')
 twoSum(numList1, 9)
-print('#'*10, 'second test')
 twoSum(numList2, 6)
-print('#'*10, 'third test')
 twoSum(numList3, 0)
-print('#'*10, 'fourth test')
-twoSum(numList4, 0)
+twoSum(numList4, 10)
