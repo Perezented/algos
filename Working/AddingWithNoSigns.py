bd82a6a211f67120c56e48efef870457caee93f0
Michael Perez <mperez6473@gmail.com>
2020-10-28 13:56:07 -0500

    Another leetcode challenge completed with folders to start organizing

---
+++ Working/AddingWithNoSigns.py
@@ -0,0 +1,13 @@
+def Calculator(a, b):
+    print('----- Currently testing: ', a, b, '-----')
+    print('----- Expect: ', a + b)
+
+    neg_string = ''
+    pos_string = ''
+
+
+Calculator(2, 1)
+# Calculator(-2, 1)
+# Calculator(-3, 1)
+# Calculator(0, 0)
+Calculator(2, 3)
